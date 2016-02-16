7029aef5a2bfb169b4b62fbdce379bc52d3abd8b
Ross Davis <Ross.ad@gmail.com>
2016-02-16 00:08:13 -0800

    Server build

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ var app = express();
 mongoose.connect('mongodb://localhost/wxalarm');
 
 // configure our server with all the middleware and routing
-require('./config/middleware.js')(app, express);
-require('./config/routes.js')(app, express);
+// require('./config/middleware.js')(app, express);
+// require('./config/routes.js')(app, express);
 
 // start listening to requests on port 8000
 app.listen(8000);
