258da63945820148164781c2bed8589e356413b7
Ross Davis <Ross.ad@gmail.com>
2016-02-15 16:00:41 -0800

    Build initial structure

---
+++ client/app/app.js
@@ -0,0 +1,3 @@
+angular.module('weatherAlarm',[
+  'weatherAlarm.services',
+  ]);
---
+++ client/app/services/services.js
@@ -0,0 +1,20 @@
+angular.module('weatherAlarm.services', [])
+.factory('Events', function($http){
+  // Get all scheduled events
+  var getAll = function(){
+    return  $http({
+      method: 'GET',
+      url: ''
+    })
+    .then();
+  };
+
+  var getOne = function(event){
+    return $http({
+      method: 'GET',
+      url: ''
+    })
+    .then();
+  };
+
+});
